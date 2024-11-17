3d49bc314b125921e6838ff93bd54def25bd202a
Yakshith15 <daggupatiyakshithnaidu@gmail.com>
2024-11-17 08:52:31 +0530

    small changes done

--- routes/routes.go
+++ routes/routes.go
@@ -1,9 +1,16 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
-import "event_management/controllers"
+import (
+    "github.com/gofiber/fiber/v2"
+    "event_management/controllers"
+    "event_management/middleware"
+)
 
-func SetUpRoutes(app *fiber.App) {
-	app.Get("/events", controllers.GetEvents)
-	app.Post("/events",controllers.CreateEvent)
+func SetupRoutes(app *fiber.App) {
+
+    app.Post("/register", controllers.Register)
+    app.Post("/login", controllers.Login)
+
+    app.Get("/events", middleware.AuthMiddleware, controllers.GetEvents)
+    app.Post("/events", middleware.AuthMiddleware, controllers.CreateEvent)
 }
